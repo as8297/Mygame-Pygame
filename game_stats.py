c1cd8f3c7d645cabf1127f9f0e9e19ced2d2733f
Vinayak Kumar <v27u27@gmail.com>
2020-10-02 08:00:49 +0530

    updated intial score

--- game_stats.py
+++ game_stats.py
@@ -15,5 +15,5 @@ class GameStats():
     def reset_stats(self):
         ''' Initialize statistics that can change during the game '''  
         self.ships_left = self.ai_settings.ship_limit
-        self.score = 0
+        self.score = -1
         self.level = 1
